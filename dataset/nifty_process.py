6cdb0e1c8e99bc0ac3e0af706f31b2f05b96cd20
Upendra Katara <chinu@Upendras-MacBook-Pro.local>
2024-04-21 10:04:43 +0530

    new files added

--- dataset/nifty_process.py
+++ dataset/nifty_process.py
@@ -1,12 +1,12 @@
 import os
 import dataset.data_act as data_act
 import pandas as pd
-import dataset.data_weather as data_weather
+import dataset.nifty_data as nifty_data
 import datetime
 from base.loss_transfer import TransferLoss
 import torch
 import math
-from dataset import data_process
+from dataset import nifty_process
 
 def load_act_data(data_folder, batch_size=64, domain="1_20"):
     x_train, y_train, x_test, y_test = data_act.load_data(data_folder, domain)
@@ -22,22 +22,22 @@ def load_act_data(data_folder, batch_size=64, domain="1_20"):
     return train_loader, train_loader, test_loader
 
 def load_nifty_data(file_path, batch_size=6):
-    data_file = os.path.join(file_path, "PRSA_Data_1.pkl")
-    mean_train, std_train = data_weather.compute_nifty_returns_statistic(data_file, start_time='2013-3-1 0:0',
-                                                                    end_time='2016-10-30 23:0')
-    train_loader = data_weather.get_nifty_data(data_file, start_time='2013-3-6 0:0',
-                                                 end_time='2015-5-31 23:0', batch_size=batch_size, mean=mean_train, std=std_train)
-    valid_train_loader = data_weather.get_nifty_data(data_file, start_time='2015-6-2 0:0',
-                                                       end_time='2016-6-30 23:0', batch_size=batch_size, mean=mean_train, std=std_train)
-    valid_vld_loader = data_weather.get_nifty_data(data_file, start_time='2016-7-2 0:0',
-                                                     end_time='2016-10-30 23:0', batch_size=batch_size, mean=mean_train, std=std_train)
-    test_loader = data_weather.get_nifty_data(data_file, start_time='2016-11-2 0:0',
-                                                end_time='2017-2-28 23:0', batch_size=batch_size, mean=mean_train, std=std_train, shuffle=False)
+    data_file = os.path.join(file_path, "nifty.pkl")
+    mean_train, std_train = nifty_data.compute_nifty_returns_statistic(data_file, start_time='2015-02-02 09:15:00',
+                                                                    end_time='2022-02-02 15:29:00')
+    train_loader = nifty_data.get_nifty_data(data_file, start_time='2015-02-02 09:15:00',
+                                                 end_time='2020-06-31 15:29:00', batch_size=batch_size, mean=mean_train, std=std_train)
+    valid_train_loader = nifty_data.get_nifty_data(data_file, start_time='2020-07-01 09:15:00',
+                                                       end_time='2022-02-02 15:29:00', batch_size=batch_size, mean=mean_train, std=std_train)
+    valid_vld_loader = nifty_data.get_nifty_data(data_file, start_time='2022-02-03 09:15:00',
+                                                     end_time='2022-03-03 15:29:00', batch_size=batch_size, mean=mean_train, std=std_train)
+    test_loader = nifty_data.get_nifty_data(data_file, start_time='2022-03-04 09:15:00',
+                                                end_time='2024-01-25 15:29:00', batch_size=batch_size, mean=mean_train, std=std_train, shuffle=False)
     return train_loader, valid_train_loader, valid_vld_loader, test_loader
 
-def get_split_time(num_domain=2, mode='pre_process', data_file=None, dis_type='coral'):
+def get_split_time(num_domain=4, mode='pre_process', data_file=None, dis_type='coral'):
     spilt_time = {
-        '2': [('2013-3-6 0:0', '2015-5-31 23:0'), ('2015-6-2 0:0', '2016-6-30 23:0')]
+        '4': [('2015-03-06 09:15:00', '2016-12-31 15:29:00'), ('2017-01-03 09:15:00', '2018-12-28 15:29:00'),('2019-01-03 09:15:00', '2020-12-28 15:29:00'),('2021-01-03 09:15:00', '2021-12-28 15:29:00')]
     }
     if mode == 'pre_process':
         return spilt_time[str(num_domain)]
@@ -48,9 +48,9 @@ def get_split_time(num_domain=2, mode='pre_process', data_file=None, dis_type='c
 
 def TDC(num_domain, data_file, dis_type='coral'):
     start_time = datetime.datetime.strptime(
-        '2013-03-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+        '2015-02-02 09:15:00', '%Y-%m-%d %H:%M:%S')
     end_time = datetime.datetime.strptime(
-        '2016-06-30 23:00:00', '%Y-%m-%d %H:%M:%S')
+        '2022-02-02 09:15:00', '%Y-%m-%d %H:%M:%S')
     num_day = (end_time - start_time).days
     split_N = 10
     data = pd.read_pickle(data_file)
@@ -94,33 +94,33 @@ def TDC(num_domain, data_file, dis_type='coral'):
         for i in range(1, len(selected)):
             if i == 1:
                 sel_start_time = start_time + datetime.timedelta(
-                    days=int(num_day / split_N * selected[i - 1]), hours=0)
+                    days=int(num_day / split_N * selected[i - 1]), hours=9, minutes=15)
             else:
                 sel_start_time = start_time + datetime.timedelta(
-                    days=int(num_day / split_N * selected[i - 1]) + 1, hours=0)
-            sel_end_time = start_time + datetime.timedelta(days=int(num_day / split_N * selected[i]), hours=23)
-            sel_start_time = datetime.datetime.strftime(sel_start_time, '%Y-%m-%d %H:%M')
-            sel_end_time = datetime.datetime.strftime(sel_end_time, '%Y-%m-%d %H:%M')
+                    days=int(num_day / split_N * selected[i - 1]) + 1, hours=9, minutes=15)
+            sel_end_time = start_time + datetime.timedelta(days=int(num_day / split_N * selected[i]), hours=15, minutes=30)
+            sel_start_time = datetime.datetime.strftime(sel_start_time, '%Y-%m-%d %H:%M:%S')
+            sel_end_time = datetime.datetime.strftime(sel_end_time, '%Y-%m-%d %H:%M:%S')
             res.append((sel_start_time, sel_end_time))
         return res
     else:
         print("error in number of domain")
 
 def load_nifty_data_multi_domain(file_path, batch_size=6, number_domain=2, mode='pre_process', dis_type='coral'):
-    data_file = os.path.join(file_path, "PRSA_Data_1.pkl")
-    mean_train, std_train = data_weather.compute_nifty_returns_statistic(data_file, start_time='2013-3-1 0:0',
+    data_file = os.path.join(file_path, "nifty_1.pkl")
+    mean_train, std_train = nifty_data.compute_nifty_returns_statistic(data_file, start_time='2013-3-1 0:0',
                                                                     end_time='2016-10-30 23:0')
     split_time_list = get_split_time(number_domain, mode=mode, data_file=data_file, dis_type=dis_type)
     train_list = []
     for i in range(len(split_time_list)):
         time_temp = split_time_list[i]
-        train_loader = data_weather.get_nifty_data(data_file, start_time=time_temp[0],
+        train_loader = nifty_data.get_nifty_data(data_file, start_time=time_temp[0],
                                                      end_time=time_temp[1], batch_size=batch_size, mean=mean_train, std=std_train)
         train_list.append(train_loader)
 
-    valid_vld_loader = data_weather.get_nifty_data(data_file, start_time='2016-7-2 0:0',
-                                                     end_time='2016-10-30 23:0', batch_size=batch_size, mean=mean_train, std=std_train)
-    test_loader = data_weather.get_nifty_data(data_file, start_time='2016-11-2 0:0',
-                                                end_time='2017-2-28 23:0', batch_size=batch_size, mean=mean_train, std=std_train, shuffle=False)
+    valid_vld_loader = nifty_data.get_nifty_data(data_file, start_time='2022-02-03 09:15:00',
+                                                     end_time='2022-03-03 15:29:00', batch_size=batch_size, mean=mean_train, std=std_train)
+    test_loader = nifty_data.get_nifty_data(data_file, start_time='2022-03-04 09:15:00',
+                                                end_time='2024-01-25 15:29:00', batch_size=batch_size, mean=mean_train, std=std_train, shuffle=False)
     return train_list, valid_vld_loader, test_loader
 
